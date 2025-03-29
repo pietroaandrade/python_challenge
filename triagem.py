a8befe361b73512b1c2d56ae0b2b3cd111adeb9d
pietroaa <167457324+pietroaandrade@users.noreply.github.com>
2025-03-29 13:56:26 -0300

    feat.basic_triagem

---
+++ triagem.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel
+
+class patient_data(BaseModel):
+    name: str
+    health_insurance: str
+
+
+
+patients = {
+
+}
+
+
+def create_pacient( ):
+    print(f"Patients in patients list: {patients}")
+    id_input = input("Enter ID (integer): ")
+    while not (id_input.isnumeric()):
+        print("invalid id")
+        id_input = input("Enter ID (integer): ")
+    if id_input.isnumeric():
+        id_input = int(id_input)
+        patients[f"{id_input}"] = ''
+        print(patients)
+    name_input = input("Enter Name: ")
+
+
+
+
+
+
